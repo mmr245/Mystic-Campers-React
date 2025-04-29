86bbfbd3aee8093b56df5d9f9d6db498c2481db0
HappyFeetLove <bexter.stormm@gmail.com>
2025-04-29 18:58:58 -0400

    linked css to app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import React from 'react';
+import './App.css';
 import Layout from './components/Layout/Layout';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/HomePage'; 
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -12,13 +12,13 @@ const About = () => {
             <p>Welcome to Mystic Campers—where the wild meets the weird! We’re more than just a camping gear company; we create immersive outdoor experiences infused with mystery and adventure. From high-quality gear to interactive scavenger hunts and VR explorations, we fuel your curiosity and invite you to embrace the unknown. Whether you seek magical discoveries or unstoppable strength, Mystic Campers is your guide to exploring the uncharted. Gear up and get exploring!</p>
         </section>
         <section>
-            <div>
+            <div className="aboutgrid">
                 <img src={BigfootBook} alt="Bigfoot reading a book" />
                 <h2><a href="lore.html">Our Lore</a></h2>
                 <p>Discover the legends that inspire our gear. From Bigfoot’s hidden trails to the glowing eyes in the
                     dark, our products are crafted with stories in mind.</p>
             </div>
-            <div>
+            <div className="aboutgrid">
                 <img src={MonsterTeam} alt="Bigfoot, Mothman, and Wendingo together" />
                 <h2><a href="team.html">Meet the Team</a></h2>
                 <p>Get to know the explorers, designers, and cryptid hunters behind Mystic Campers.</p>
