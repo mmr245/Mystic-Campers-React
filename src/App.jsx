406efda21f5ddd168b5c872ca5e5213c17f54047
andreanne973 <andreanne942@gmail.com>
2025-04-28 23:58:43 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 
 import React from 'react';
 import Layout from './components/Layout/Layout';
-
 import { Routes, Route } from 'react-router-dom';
-
-import Home from './pages/HomePage';  // import your HomePage
-
+import Home from './pages/HomePage'; 
 import Header from './components/Layout/Header';
 import Shop from './pages/ShopPage';
 import About from './pages/AboutPage';
@@ -19,15 +16,10 @@ function App() {
   return (
     <Layout>
       <Routes>
-        {/* Add the home route here */}
         <Route path="/" element={<Home />} />
-
-        {/* existing routes */}
         <Route path="/about" element={<About />} />
         <Route path="/hunt" element={<Hunt />} />
         <Route path="/cart" element={<Cart />} />
-
-        {/* optional: add a catch-all route for 404 */}
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Layout>
