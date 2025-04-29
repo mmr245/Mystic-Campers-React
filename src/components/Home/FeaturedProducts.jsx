ba283dedd8af73b16f6ae76769c1028bde059e1b
andreanne973 <andreanne942@gmail.com>
2025-04-29 01:54:43 -0400

    Update FeaturedProducts.jsx

--- src/components/Home/FeaturedProducts.jsx
+++ src/components/Home/FeaturedProducts.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-// Import your product images
+// Import images
 import babaYaga from '../../assets/images/catalog_images/baba-yaga-cookset.png';
 import banshee from '../../assets/images/catalog_images/banshee-repel.png';
 import cerberus from '../../assets/images/catalog_images/cerberus-flashlight.png';
 import chup from '../../assets/images/catalog_images/chup-repel.png';
 
-const featuredProducts = [
+const featured = [
   { id: 1, name: 'Baba Yaga Cookset', price: '$45.00', image: babaYaga },
   { id: 2, name: 'Banshee Reppel', price: '$24.00', image: banshee },
   { id: 3, name: 'Cerberus Flashlight', price: '$35.00', image: cerberus },
@@ -13,15 +13,15 @@ const featuredProducts = [
 ];
 
 const FeaturedProducts = () => (
-  <div className="featured-products">
-    {featuredProducts.map((product) => (
-      <div key={product.id} className="product-card">
-        <img src={product.image} alt={product.name} />
-        <h3>{product.name}</h3>
-        <p>{product.price}</p>
+  <>
+    {featured.map((p) => (
+      <div key={p.id} className="product-card">
+        <img src={p.image} alt={p.name} />
+        <h3>{p.name}</h3>
+        <p>{p.price}</p>
       </div>
     ))}
-  </div>
+  </>
 );
 
 export default FeaturedProducts;
